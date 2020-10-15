2ca942cbb8acfe8a371a373e4afcaa73026511b2
AndreySVRF <andreysmagin14@gmail.com>
2020-10-15 23:57:26 +0400

    Day 1 time: 1:15

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAI2Fg8pfid_FFohHqjBjBjED769Hlftrs",
+  authDomain: "react-whats-app-clone.firebaseapp.com",
+  databaseURL: "https://react-whats-app-clone.firebaseio.com",
+  projectId: "react-whats-app-clone",
+  storageBucket: "react-whats-app-clone.appspot.com",
+  messagingSenderId: "313629974526",
+  appId: "1:313629974526:web:4e813f903fdcbe6079eaaf"
+};
